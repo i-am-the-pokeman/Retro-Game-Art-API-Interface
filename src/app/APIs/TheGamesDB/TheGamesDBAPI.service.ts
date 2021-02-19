9c533d793fb240d6e56d23e6846b2e4e6c218842
i-am-the-pokeman <joseph.r.bayer@gmail.com>
2021-02-19 13:18:00 -0600

    Add API call for getGamesByPlatformId

--- src/app/APIs/TheGamesDB/TheGamesDBAPI.service.ts
+++ src/app/APIs/TheGamesDB/TheGamesDBAPI.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { GETPlatformsRequest, TheGamesDBBaseUrl } from './TheGamesDBAPIEntities';
+import { GETGamesByPlatformIdRequest, GETGamesByPlatformIdResponse, GETPlatformsRequest, GETPlatformsResponse, TheGamesDBBaseUrl } from './TheGamesDBAPIEntities';
 import { tap } from 'rxjs/operators';
 import { APIUtils } from '../API-utils';
-import { of } from 'rxjs';
+import { Observable, of } from 'rxjs';
 const Store = window.require('electron-store');
 const electronStore = new Store({accessPropertiesByDotNotation: false});
 
@@ -12,7 +12,7 @@ export class TheGamesDBAPIService {
 
   constructor(private http: HttpClient) { }
 
-  getAllPlatforms(request: GETPlatformsRequest) {
+  getAllPlatforms(request: GETPlatformsRequest): Observable<GETPlatformsResponse> {
     // TODO: create a reusable way to generate GET requests with query params
     let params = new HttpParams();
     params = params.append('apikey', request.apikey);
@@ -22,7 +22,26 @@ export class TheGamesDBAPIService {
     if (electronStore.has(url)) {
       return of(electronStore.get(url));
     } else {
-      return this.http.get(url)
+      return this.http.get<GETPlatformsResponse>(url)
+              .pipe(
+                tap((response) => {
+                  electronStore.set(`${url}`, response);
+                })
+              );
+    }
+  }
+
+  getGamesByPlatformId(request: GETGamesByPlatformIdRequest): Observable<GETGamesByPlatformIdResponse> {
+    let params = new HttpParams();
+    params = params.append('apikey', request.apikey);
+    params = params.append('id', request.id);
+  
+    let url = APIUtils.buildUrlWithQueryParams(TheGamesDBBaseUrl + 'v1/Games/ByPlatformID', params);
+
+    if (electronStore.has(url)) {
+      return of(electronStore.get(url));
+    } else {
+      return this.http.get<GETGamesByPlatformIdResponse>(url)
               .pipe(
                 tap((response) => {
                   electronStore.set(`${url}`, response);
--- src/app/APIs/TheGamesDB/TheGamesDBAPIEntities.ts
+++ src/app/APIs/TheGamesDB/TheGamesDBAPIEntities.ts
@@ -1,28 +1,115 @@
 export const TheGamesDBBaseUrl = 'https://api.thegamesdb.net/';
 
-export interface GETPlatformsRequest {
-    apikey: string;
-    fields?: string;
+export interface BaseApiResponse {
+  code: number;
+  status: string;
+  remaining_monthly_allowance: number;
+  extra_allowance: number;
 }
-export interface GETPlatformsRsponse {
-    code: number;
-    status: string;
-    remaining_monthly_allowance: number;
 
-    data: GetPlatformsData;
+// DTOs
+export interface GETPlatformsRequest {
+  apikey: string;
+  fields?: string;
+}
+export interface GETPlatformsResponse extends BaseApiResponse {
+  data: GetPlatformsData;
 }
 export interface GetPlatformsData {
-    count: number;
-    platforms: Platform[];
+  count: number;
+  platforms: PlatformsDictionary;
+}
+export interface PlatformsDictionary {
+    [key: string]: Platform;
 }
 
-export interface Platform {
-    id: number;
-    name: string;
-    alias: string;
-    icon: string;
-    console: string;
-    controller: string;
-    developer: string;
-    overview: string;
+// ---
+
+export interface GETGamesByPlatformIdRequest {
+  apikey: string;
+  id: string;
+  fields?: string;
+  include?: string;
+  page?: number;
 }
+export interface GETGamesByPlatformIdResponse extends GETGamesByGameIdResponse {}
+
+// --
+
+export interface GETGamesByGameIdResponse extends BaseApiResponse {
+    data: GetGamesData;
+}
+export interface GetGamesData {
+    count: number;
+    data: Game[];
+}
+
+// --
+
+export interface GETGameImagesByGameIdRequest {
+    apikey: string;
+    games_id: string;   
+}
+export interface GETGameImagesByGameIdResponse extends BaseApiResponse {
+    data: GetGameImagesData
+}
+export interface GetGameImagesData {
+    count: number;
+    base_url: ImageBaseUrlMeta;
+    images: GameImage[];
+}
+
+// END DTOs
+
+// Common Models
+export interface Platform {
+  id: number;
+  name: string;
+  alias: string;
+  icon: string;
+  console: string;
+  controller: string;
+  developer: string;
+  overview: string;
+}
+
+export interface Game {
+  id: number;
+  game_title: string;
+  release_date: string;
+  platform: number;
+  players: number;
+  overview: string;
+  last_updated: string;
+  rating: string;
+  coop: string;
+  youtube: string;
+  os: string;
+  processor: string;
+  ram: string;
+  hdd: string;
+  video: string;
+  sound: string;
+  developers: number[];
+  genres: number[];
+  publishers: number[];
+  alternates: string[];
+}
+
+export interface ImageBaseUrlMeta {
+    original: string;
+    small: string;
+    thumb: string;
+    cropped_center_thumb: string;
+    medium: string;
+    large: string;
+}
+
+export interface GameImage {
+    id: number;
+    type: string;
+    side: string;
+    filename: string;
+    resolution: string;
+}
+// END Common Models
