5012e30490fad1b954d78464848f45a264b5da19
i-am-the-pokeman <joseph.r.bayer@gmail.com>
2021-02-20 12:04:53 -0600

    Remove unused modules

--- src/app/api-interface-screen-one/game-selection/game-selection.module.ts
+++ src/app/api-interface-screen-one/game-selection/game-selection.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { GameSelectionComponent } from './game-selection.component';
-import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatInputModule } from '@angular/material/input';
-import { MatIconModule } from '@angular/material/icon';
-import { MatGridListModule } from '@angular/material/grid-list';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -15,13 +12,10 @@ import { BrowserModule } from '@angular/platform-browser';
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    MatCardModule,
     MatFormFieldModule,
     MatSelectModule,
     MatAutocompleteModule,
     MatInputModule,
-    MatIconModule,
-    MatGridListModule
   ],
   declarations: [
     GameSelectionComponent,
