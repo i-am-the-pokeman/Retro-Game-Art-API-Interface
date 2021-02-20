7f9d25354906901cc7b2ec873d954905194a265e
i-am-the-pokeman <joseph.r.bayer@gmail.com>
2021-02-20 15:07:01 -0600

    Remove reference to deleted file and fix spacing between buttons

--- src/app/shared/components/next-back-stepper-buttons/next-back-stepper-buttons.component.ts
+++ src/app/shared/components/next-back-stepper-buttons/next-back-stepper-buttons.component.ts
@@ -2,8 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'next-back-stepper-buttons',
-  templateUrl: './next-back-stepper-buttons.component.html',
-  styleUrls: ['./next-back-stepper-buttons.component.less']
+  templateUrl: './next-back-stepper-buttons.component.html'
 })
 export class NextBackStepperButtonsComponent{
 
