3c78d621fda7759d38e4989f924f7182b51747b6
i-am-the-pokeman <joseph.r.bayer@gmail.com>
2021-02-23 16:25:40 -0600

    REmove unnecessary imports

--- src/app/api-interface-screen-one/download-selection/download-selection.module.ts
+++ src/app/api-interface-screen-one/download-selection/download-selection.module.ts
@@ -1,25 +1,11 @@
 import { NgModule } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatInputModule } from '@angular/material/input';
-import { MatIconModule } from '@angular/material/icon';
-import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { DownloadSelectionComponent } from './download-selection.component';
 
 
 @NgModule({
   imports: [
-    BrowserModule,
-    ReactiveFormsModule,
-    MatCardModule,
-    MatFormFieldModule,
-    MatSelectModule,
-    MatAutocompleteModule,
-    MatInputModule,
-    MatIconModule
+    BrowserModule
   ],
   declarations: [
     DownloadSelectionComponent,
