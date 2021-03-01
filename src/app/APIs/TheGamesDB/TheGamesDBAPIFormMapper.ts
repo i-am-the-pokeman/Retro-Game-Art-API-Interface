05b602b1227543564945cfc7f1de975d5a14bc34
i-am-the-pokeman <joseph.r.bayer@gmail.com>
2021-03-01 13:05:35 -0600

    Distinguish duplicate text options by providing id

--- src/app/APIs/TheGamesDB/TheGamesDBAPIFormMapper.ts
+++ src/app/APIs/TheGamesDB/TheGamesDBAPIFormMapper.ts
@@ -33,33 +33,44 @@ export class TheGamesDBAPIFormMapper {
       return games.map(game => ({Text: game.game_title, Value: game}));
     }
 
+    //#region Game Image Type Dropdown Functions
     // TODO: add unit test
     public static MapGameImagesDictionaryToImageTypeDropdownOptions(imagesDictionary: Dictionary) {
       let dictionaryValueArray: GameImage[][] = this.GetDictionaryValues(imagesDictionary);
       let dictionaryValues: GameImage[] = [];
-      if(dictionaryValueArray.length) { // Since we only call for one game's image set at a time, we only need the first result
+      if (dictionaryValueArray.length) { // Since we only call for one game's image set at a time, we only need the first result
         dictionaryValues = dictionaryValueArray[0];
       }
 
       let dropdownOptions = this.mapGameImagesToImageTypeDropdownOptions(dictionaryValues);
+      console.log(dropdownOptions);
       return dropdownOptions;
     }
     private static mapGameImagesToImageTypeDropdownOptions(gameImages: GameImage[] = []): DropdownOption[] {
-      return gameImages.map(gameImage => ({
-                                            Text: this.mapGameImageToImageTypeDropdownText(gameImage),
-                                            Value: gameImage
-                                          })
-                            )
-                        .sort((a, b) => this.sortByGameImageTypePriority(a.Value, b.Value));
+      return gameImages.map(gameImage =>
+                        {
+                          return {
+                              Text: this.mapGameImageToImageTypeDropdownText(gameImage, gameImages),
+                              Value: gameImage
+                            }
+                        })
+                        .sort((a, b) => this.sortGameImageTypes(a.Value, b.Value));
     }
-    private static mapGameImageToImageTypeDropdownText(gameImage: GameImage) {
+    private static mapGameImageToImageTypeDropdownText(gameImage: GameImage, gameImageList: GameImage[]) {
       let type = (this.GameImageTypeDisplayValueMap.has(gameImage.type))
                   ? this.GameImageTypeDisplayValueMap.get(gameImage.type)
                   : this.TitleCasePipe.transform(gameImage.type);
-      let side = this.TitleCasePipe.transform(gameImage.side);
-      return (side?.length)
-              ? type + ' (' + side + ')'
-              : type;
+
+      let side = (gameImage?.side?.length)
+                  ? ' (' + this.TitleCasePipe.transform(gameImage.side) + ')'
+                  : '';
+
+      let id = '';
+      if (gameImageList.filter((i) => i.type === gameImage.type && i.side === gameImage.side).length > 1) {
+        id = ' (' + gameImage.id + ')';
+      }
+
+      return type + side + id;
     }
     private static GameImageTypeDisplayValueMap = new Map<string, string>([
       ['fanart', 'Fan Art'],
@@ -81,18 +92,29 @@ export class TheGamesDBAPIFormMapper {
       ['front', 1],
       ['back', 2],
     ]);
+    private static sortGameImageTypes(a: GameImage, b: GameImage): number {
+      if (this.GameImageTypePriorityMap.get(a.type) !== this.GameImageTypePriorityMap.get(b.type)) {
+        return this.sortByGameImageTypePriority(a, b);
+      } else if (this.GameImageTypeSidePriorityMap.get(a.side) !== this.GameImageTypeSidePriorityMap.get(b.side)) {
+        return this.sortByGameImageTypeSidePriority(a, b);
+      } else {
+        return this.sortByGameImageTypeId(a, b);
+      }
+    }
     private static sortByGameImageTypePriority(a: GameImage, b: GameImage): number {
       return (this.GameImageTypePriorityMap.get(a.type) > this.GameImageTypePriorityMap.get(b.type))
               ? 1
-              : this.GameImageTypePriorityMap.get(a.type) === this.GameImageTypePriorityMap.get(b.type)
-                ? this.sortByGameImageTypeSidePriority(a, b)
-                : -1;
+              : -1;
     }
     private static sortByGameImageTypeSidePriority (a: GameImage, b: GameImage): number {
       return (this.GameImageTypeSidePriorityMap.get(a.side) > this.GameImageTypeSidePriorityMap.get(b.side))
               ? 1
               : -1;
     }
+    private static sortByGameImageTypeId (a: GameImage, b: GameImage): number {
+      return (a.id > b.id) ? 1 : -1;
+    }
+    //#endregion
 
     // TODO: move this somewhere generic...
     public static GetDictionaryValues(dictionary: any): any[] {
