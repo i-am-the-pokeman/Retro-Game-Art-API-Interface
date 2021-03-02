14d9c86782662490f971b0f1dc3e233a135f6747
i-am-the-pokeman <joseph.r.bayer@gmail.com>
2021-03-01 21:59:10 -0600

    Add debounce time and reset control instead of  setting to default value

--- src/app/shared/components/easy-autocomplete/easy-autocomplete.component.ts
+++ src/app/shared/components/easy-autocomplete/easy-autocomplete.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable, Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { debounceTime, takeUntil } from 'rxjs/operators';
 import { AngularMaterialAutocompleteUtils } from '../../form-helpers/angular-material-autocomplete-utils';
 import { DropdownOption, FormInputData } from '../../form-helpers/entities';
 
@@ -22,13 +22,16 @@ export class EasyAutocompleteComponent implements OnInit, OnDestroy {
     this._dropdownOptions = options;
 
     this.filteredDropdownOptions
-      = AngularMaterialAutocompleteUtils.GetFilteredAutoCompleteOptions$(this.autocompleteFormControl, this.dropdownOptions)
+      = AngularMaterialAutocompleteUtils
+        .GetFilteredAutoCompleteOptions$(this.autocompleteFormControl, this.dropdownOptions)
         .pipe(takeUntil(this.stop$));
   }
   private _dropdownOptions: DropdownOption[] = [];
   filteredDropdownOptions: Observable<DropdownOption[]> = new Observable<DropdownOption[]>();
 
   errorMessage: string = '';
+
+  pingCount: number = 0;
 
   private stop$ = new Subject<any>();
 
@@ -38,8 +41,12 @@ export class EasyAutocompleteComponent implements OnInit, OnDestroy {
     // Note: we must initialize the error message here, because touching + blurring an input for the first time does not fire statusChanges
     this.errorMessage = this.buildErrorMessage();
     this.autocompleteFormControl.statusChanges
-      .pipe(takeUntil(this.stop$))
-      .subscribe(() => {
+      .pipe(
+        takeUntil(this.stop$),
+        debounceTime(500)
+      ).subscribe((status) => {
+        console.log(status);
+        console.log('ping ' + ++this.pingCount);
         this.errorMessage = this.buildErrorMessage();
       });
   }
@@ -50,7 +57,7 @@ export class EasyAutocompleteComponent implements OnInit, OnDestroy {
   }
 
   resetFormControl() {
-    this.autocompleteFormControl.setValue(this.formInputData.DefaultValue);
+    this.autocompleteFormControl.reset();
   }
 
   private buildErrorMessage(): string {
