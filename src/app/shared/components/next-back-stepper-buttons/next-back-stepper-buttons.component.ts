b4c836c143b90c4253eaa07b145ee8602b7add2d
i-am-the-pokeman <joseph.r.bayer@gmail.com>
2021-03-08 21:52:32 -0600

    Make next/back stepper buttons bigger

--- src/app/shared/components/next-back-stepper-buttons/next-back-stepper-buttons.component.ts
+++ src/app/shared/components/next-back-stepper-buttons/next-back-stepper-buttons.component.ts
@@ -2,7 +2,8 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'next-back-stepper-buttons',
-  templateUrl: './next-back-stepper-buttons.component.html'
+  templateUrl: './next-back-stepper-buttons.component.html',
+  styleUrls: ['./next-back-stepper-buttons.component.sass']
 })
 export class NextBackStepperButtonsComponent{
 
