0fc60448b904ae23f86d9b6cbe849ddf33dcca17
i-am-the-pokeman <joseph.r.bayer@gmail.com>
2021-05-29 18:30:42 -0500

    Add comments to main.ts to explain what's going on. Also added a cleanup TODO

--- electron/main.ts
+++ electron/main.ts
@@ -3,12 +3,13 @@ import { app, BrowserWindow, ipcMain } from "electron";
 const Store = require('electron-store');
 Store.initRenderer();
 
+// Initialize App by Creating WindowService Singleton
 const WindowService = require('./services/WindowServices/WindowService');
+
+// Initialize Listeners with MessageHandler Singletons
 const ImageDownloadMessagehandlers = require('./message-handlers/ImageDownloadMessageHandlers');
 
-// APP EVENTS
-
-// END APP EVENTS
+// TODO: clean up below with new message handler class
 
 // IPC EVENTS
 
